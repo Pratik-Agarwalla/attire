979fc869fb9410c86f8cc934373f320f163950f3
Pratik Agarwalla <pratikagarwalla2003@gmail.com>
2025-01-16 15:11:05 +0530

    converted to backend server

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://attire-clothing.vercel.app",
+  "https://attire-fashion.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.send("Server is up and running!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
