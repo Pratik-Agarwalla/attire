89cef3f2a3bbd8eb4bea03faecb798de1a4a9d51
Pratik Agarwalla <pratikagarwalla2003@gmail.com>
2025-01-16 15:59:33 +0530

    script added

--- server.js
+++ server.js
@@ -32,5 +32,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at PORT:${port}`);
 });
